66eabbf44681c3522ed3bffeadfa17473cf6c4dd
Ystvlxst <88839467+Ystvlxst@users.noreply.github.com>
2021-12-11 11:17:35 +0500

    Update HealthBar.cs

--- HealthBar.cs
+++ HealthBar.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHealthBar : MonoBehaviour
+public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
     private HealthValue _healthValue;
 
     private void Start()
     {
-        _slider.value = _healthValue.PlayerHealthValue;
+        _slider.value = _healthValue.HealthValue;
     }
 }
