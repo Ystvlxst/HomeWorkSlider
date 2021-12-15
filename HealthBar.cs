3c46bbdec0c8b1f34945813303d558e6b1a55e87
Ystvlxst <88839467+Ystvlxst@users.noreply.github.com>
2021-12-15 12:12:15 +0500

    Update HealthBar.cs

--- HealthBar.cs
+++ HealthBar.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour
+public class PlayerHealthBar : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
     private HealthValue _healthValue;
 
     private void Start()
     {
-        _slider.value = _healthValue.HealthValue;
+        _slider.value = _healthValue.PlayerHealthValue;
+    }
+
+    private void Hit()
+    {
+        _healthValue.Hit();
+        StopCoroutine(ChangeSliderValue(_healthValue.ChangedHealthValue));
+        StartCoroutine(ChangeSliderValue(-_healthValue.ChangedHealthValue));
+    }
+
+    private void Cure()
+    {
+        _healthValue.Cure();
+        StopCoroutine(ChangeSliderValue(-_healthValue.ChangedHealthValue));
+        StartCoroutine(ChangeSliderValue(_healthValue.ChangedHealthValue));
+    }
+
+    public IEnumerator ChangeSliderValue(float targetValue)
+    {
+        float maxDelta = 0.1f;
+
+        while (_slider.value != targetValue)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, targetValue, maxDelta);
+            yield return null;
+        }
     }
 }
